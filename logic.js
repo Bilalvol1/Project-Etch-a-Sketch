49c092460e22c10380ced51e3e6be6c5ad88c549
Bilal Shaikh <vol1shaikh@gmail.com>
2025-06-30 15:24:37 +0530

    Add 16*16 grid

---
+++ logic.js
@@ -0,0 +1,12 @@
+const container = document.querySelector("#container");
+
+let num = 16;
+let gridSize = num * num;
+let grid;
+
+for (let i = 1; i <= gridSize; i++) {
+    grid = document.createElement("div");
+    grid.classList.add("grid")
+    grid.setAttribute("style", `flex-basis: ${100/num}%`)
+    container.appendChild(grid);
+}
