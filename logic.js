7a293882791036993d6aecc837af5f92a1cc3c03
Bilal Shaikh <vol1shaikh@gmail.com>
2025-06-30 16:08:50 +0530

    Add hover effect

--- logic.js
+++ logic.js
@@ -10,3 +10,11 @@ for (let i = 1; i <= gridSize; i++) {
     grid.setAttribute("style", `flex-basis: ${100/num}%`)
     container.appendChild(grid);
 }
+
+const grids = document.querySelectorAll(".grid");
+
+grids.forEach((grid) => {
+    grid.addEventListener("mouseenter", () => {
+        grid.classList.add("hover")
+    })
+})
